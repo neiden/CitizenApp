6a052fb7ad8ba3cb7dd36550ca26041f57057a31
John <swanbej3@wwu.edu>
2021-06-01 23:38:25 -0700

    Moved feed to FeedActivity

--- app/src/main/java/com/example/citizen/MainActivity.kt
+++ app/src/main/java/com/example/citizen/MainActivity.kt
@@ -8,47 +8,16 @@ import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity(), PostDialogListener {
-    lateinit var newPostButton: Button
+class MainActivity : AppCompatActivity() {
 
-    companion object{
-        var postNumber = 0
-        var POSTLIST: MutableList<Post> = mutableListOf()
-    }
-
-    override fun onPassData(data: String) {
-        var postList: RecyclerView = findViewById(R.id.postList)
-        var dataArray = data.split(",").toTypedArray()
-        var op = dataArray[0]
-        var title = dataArray[2]
-        var description = dataArray[1]
-        var location = dataArray[3]
-        POSTLIST.add(Post("$op", "$description", "$title", "$location"))
-        postList.adapter?.notifyDataSetChanged()
-        postNumber++
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+          super.onCreate(savedInstanceState)
+          setContentView(R.layout.activity_main)
 
+          var intent = Intent(this, FeedActivity::class.java)
+          this.startActivity(intent)
 
-        var postList: RecyclerView = findViewById(R.id.postList)
-        postList.adapter = MyPostRecyclerViewAdapter(POSTLIST, this)
-        postList.layoutManager = LinearLayoutManager(this)
-
-        POSTLIST.add(Post("John42069", "This is fucked", "Help me", "Seattle, WA"))
-        postList.adapter?.notifyDataSetChanged()
-        postNumber++
-
-        newPostButton = findViewById(R.id.addPost)
-        newPostButton.setOnClickListener{
-            var postDialogFrag = CreatePostDialogFragment()
-            postDialogFrag.show(supportFragmentManager, "fragment_create_post_dialog")
-        }
-
-        //var intent = Intent(this, PostActivity::class.java)
-        //startActivity(intent)
     }
 
 
